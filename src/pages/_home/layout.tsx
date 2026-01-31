c0a74276bb3426fa40dac8417f8baeae4231ccf9
Gabriel Silva Santos <gabrielss432112@gmail.com>
2026-01-30 21:13:21 -0300

    feat: add logout

--- src/pages/_home/layout.tsx
+++ src/pages/_home/layout.tsx
@@ -3,11 +3,21 @@ import {
 	Link,
 	Outlet,
 	redirect,
+	useNavigate,
 } from '@tanstack/react-router';
+import { LogOut } from 'lucide-react';
+import { toast } from 'sonner';
 import { ThemeToggler } from '@/components/theme/theme-toggler';
+import { Button } from '@/components/ui/button';
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from '@/components/ui/tooltip';
 import { getMeQueryOptions } from '@/http/requests/authentication';
 import {
 	authStore,
+	logout,
 	useAuth,
 } from '@/integrations/tanstack-store/stores/auth.store';
 
@@ -36,6 +46,16 @@ export const Route = createFileRoute('/_home')({
 
 function Layout() {
 	const { user } = useAuth();
+	const navigate = useNavigate();
+
+	const onLogout = () => {
+		logout();
+		toast.success('Logged out', {
+			description: 'You have been logged out successfully.',
+		});
+		navigate({ to: '/login', replace: true });
+	};
+
 	return (
 		<>
 			<div className="flex items-center justify-between p-2">
@@ -47,8 +67,23 @@ function Layout() {
 						About
 					</Link>
 				</div>
-				<div className="flex items-center">
-					{user && <span className="mr-4">Hello, {user.username}</span>}
+				<div className="flex items-center gap-4">
+					{user && (
+						<>
+							<span className="mr-4">Hello, {user.username}</span>
+							<Tooltip>
+								<TooltipTrigger asChild>
+									<Button variant="outline" onClick={onLogout}>
+										<LogOut strokeWidth={2.5} />
+										<span className="sr-only">Logout</span>
+									</Button>
+								</TooltipTrigger>
+								<TooltipContent side="bottom">
+									<p>Logout</p>
+								</TooltipContent>
+							</Tooltip>
+						</>
+					)}
 					<ThemeToggler />
 				</div>
 			</div>
