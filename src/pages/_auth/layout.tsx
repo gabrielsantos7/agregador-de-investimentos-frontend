118acda8ce899e1804c5f3c78899eb73a421f061
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2026-01-30 21:47:20 -0300

    fix: center auth layout container

--- src/pages/_auth/layout.tsx
+++ src/pages/_auth/layout.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/_auth')({
 
 function Layout() {
 	return (
-		<div className="container min-h-dvh flex items-center justify-center px-4 bg-linear-to-br from-background/50 via-background to-background/50">
+		<div className="container mx-auto min-h-dvh flex items-center justify-center px-4 bg-linear-to-br from-background/50 via-background to-background/50">
 			<Outlet />
 		</div>
 	);
